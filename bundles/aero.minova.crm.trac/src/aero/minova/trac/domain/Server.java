1f8417bb0db97bea7eb26aa5705fa011e2c606cf
Wilfried Saak <saak@minova.aero>
2021-12-23 11:55:04 +0100

    Formatierten WikiText vom Trac Server leden

--- bundles/aero.minova.crm.trac/src/aero/minova/trac/domain/Server.java
+++ bundles/aero.minova.crm.trac/src/aero/minova/trac/domain/Server.java
@@ -140,6 +140,19 @@ public class Server {
 	}
 
 	/**
+	 * Liest den Inhalt der angegebenen Wiki-Seite
+	 * 
+	 * @param wikiAddress
+	 *            die (interne) Adresse der Wiki-Seite, z.B. "Module/ch.minova.sap.sales"
+	 * @return {@link Wiki}
+	 */
+	public String wikiTiHtml(String wikiText) {
+		aero.minova.trac.xmlprc.Wiki wiki = (aero.minova.trac.xmlprc.Wiki) trackerDynamicProxy.newInstance(aero.minova.trac.xmlprc.Wiki.class);
+		String html = wiki.wikiToHtml(wikiText);
+		return html;
+	}
+
+	/**
 	 * Schreibt den Inhalt auf die angegebene Wiki-Seite.
 	 * 
 	 * @param internalWiki
